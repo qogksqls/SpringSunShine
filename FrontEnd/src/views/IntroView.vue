bdc0906605e03ff1f64f2c43dbfb6c6e93fef787
qoqls1212 <qoqls1212@naver.com>
2022-07-24 15:03:01 +0900

    Intro 화면 추가

---
+++ FrontEnd/src/views/IntroView.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-carousel>
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    ></v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          },
+        ],
+      }
+    },
+  }
+</script>
+
+<style>
+.v-window__next {
+  background:rgba(0,0,0,0.15);
+  position: absolute;
+  height:68px;
+  width:68px;
+  top:calc(50% - 33px);
+  margin-right:13%;
+}
+.v-window__prev {
+  background:rgba(0,0,0,0.15);
+  position: absolute;
+  height:68px;
+  width:68px;
+  top:calc(50% - 33px);
+  margin-left:13%;
+}
+</style>
