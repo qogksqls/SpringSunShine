a9bb4a97626314ee1aa1def274a00d3c675367f4
qoqls1212 <qoqls1212@naver.com>
2022-07-24 15:03:34 +0900

    임시로 홈 & 소개 comp 연결

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -1,15 +1,31 @@
 <template>
   <div>
     <nav-bar-comp></nav-bar-comp>
+    <home-view></home-view>
+    <intro-view></intro-view>
   </div>
 </template>
 
 <script>
   import NavBarComp from './components/NavBarComp.vue'
+  import HomeView from '@/views/HomeView.vue'
+  import IntroView from '@/views/IntroView.vue'
+
 
   export default {
     components: {
       NavBarComp,
+      HomeView,
+      IntroView,
     },
   }
 </script>
+
+<style scoped>
+@font-face {
+    font-family: 'GangwonEdu_OTFBoldA';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/GangwonEdu_OTFBoldA.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+</style>
