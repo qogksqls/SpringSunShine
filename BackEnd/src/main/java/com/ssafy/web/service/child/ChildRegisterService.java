24820a66e026fb3ffd17fcbd305bf74f0a7bcb29
김규란 <gyuraniakim@naver.com>
2022-07-27 13:23:44 +0900

    Feat: child-be
    내 아이 등록 작성 중

---
+++ BackEnd/src/main/java/com/ssafy/web/service/child/ChildRegisterService.java
@@ -0,0 +1,10 @@
+package com.ssafy.web.service.child;
+
+import com.ssafy.web.request.child.ChildRegisterRequest;
+
+public interface ChildRegisterService {
+
+	// 아이 등록
+	void childRegist(ChildRegisterRequest childInfo);
+
+}
