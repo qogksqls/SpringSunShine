ff2821dcc52543fb3f560e572708c40d3da10280
최지혜 <chlwlgp11@naver.com>
2022-08-02 15:24:31 +0900

    Feat:register-be

--- BackEnd/src/main/java/com/ssafy/web/controller/MailController.java
+++ BackEnd/src/main/java/com/ssafy/web/controller/MailController.java
@@ -1,5 +1,7 @@
 package com.ssafy.web.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.web.request.EmailRequest;
 import com.ssafy.web.service.MailService;
 
 @RestController
@@ -18,10 +21,26 @@ public class MailController {
 	MailService mailService;
 	
 	@PostMapping
-	public ResponseEntity<?> sendEmail(@RequestBody String email) throws Exception{
-		System.out.println(email);
-		mailService.sendMail(email);
+	public ResponseEntity<?> sendEmail(@RequestBody EmailRequest email) throws Exception{
+		String myemail = email.getEmail();
+		System.out.println("메일 보낼 이메일 : "+myemail);
+		String re ="fail";
+		try{
+			re = mailService.sendSimpleMessage(myemail);
+		}catch(Exception e) {
+			return new ResponseEntity<String>("fail", HttpStatus.NO_CONTENT);
+		}
 		
-		return new ResponseEntity<String>("sendSuccess", HttpStatus.OK);
+		return new ResponseEntity<String>(re, HttpStatus.OK);
 	}
+	
+//	@PostMapping("/verify")
+//	public int verifyCode(@RequestBody Map<String, String> codedate) {
+//		System.out.println("넘어온 인증번호 : "+codedate.get("code"));
+//		int result=0;
+//		if(MailService.ssspass.equals(codedate.get("code"))){
+//			result = 1;
+//		}
+//		return result;
+//	}
 }
