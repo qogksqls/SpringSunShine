d80bc9a1573dc3b55190edcad77505f1d5cc5d6d
Lee Bom <Sbome0214@naver.com7>
2022-08-04 23:06:35 +0900

    FEAT| store 추가

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -18,6 +18,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import store from "./store";
 import Argon from "./plugins/argon-kit";
 import "./registerServiceWorker";
 
@@ -25,5 +26,6 @@ Vue.config.productionTip = false;
 Vue.use(Argon);
 new Vue({
   router,
+  store,
   render: (h) => h(App),
 }).$mount("#app");
