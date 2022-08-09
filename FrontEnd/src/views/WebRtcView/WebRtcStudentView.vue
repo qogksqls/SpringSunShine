919955837e747978ddabc6a526454f96af16c83a
JunOnJuly <wlgnstjd0717@gmail.com>
2022-08-09 18:27:18 +0900

    FEAT|update

--- FrontEnd/src/views/WebRtcView/WebRtcStudentView.vue
+++ FrontEnd/src/views/WebRtcView/WebRtcStudentView.vue
@@ -78,8 +78,7 @@ import SubVideoComp from './SubVideoComp.vue'
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-const OPENVIDU_SERVER_URL = "https://" + location.hostname + ":4443";
-// const OPENVIDU_SERVER_URL = "https://a606.shop:8443" ;
+const OPENVIDU_SERVER_URL = "https://i7a606.q.ssafy.io:8443" ;
 
 const OPENVIDU_SERVER_SECRET = "MY_SECRET";
 
