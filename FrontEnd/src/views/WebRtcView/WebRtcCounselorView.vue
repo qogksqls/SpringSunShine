87de6010be64e047974f59b7b5514c8e3807cdbd
JunOnJuly <wlgnstjd0717@gmail.com>
2022-08-09 18:18:46 +0900

    FEAT|please

--- FrontEnd/src/views/WebRtcView/WebRtcCounselorView.vue
+++ FrontEnd/src/views/WebRtcView/WebRtcCounselorView.vue
@@ -123,9 +123,9 @@ import SubVideoComp from './SubVideoComp.vue'
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 // const OPENVIDU_SERVER_URL = "https://" + location.hostname + ":4443";
-const OPENVIDU_SERVER_URL = "https://i7a606.q.ssafy.io:8443/dashboard" ;
+const OPENVIDU_SERVER_URL = "https://i7a606.q.ssafy.io:8443" ;
 
-const OPENVIDU_SERVER_SECRET = "MY_SECRET";
+const OPENVIDU_SERVER_SECRET = "A606";
 
 export default {
   watch: {
