eb98268f271252270904c8e432bacf7c519655dd
최지혜 <chlwlgp11@naver.com>
2022-08-10 19:32:08 +0900

    FEAT:be-questions
    question

---
+++ BackEnd-Service/src/main/java/com/ssafy/web/dto/Answerlist.java
@@ -0,0 +1,16 @@
+package com.ssafy.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Answerlist {
+	int question; //질문번호
+	int answer; // 답 
+
+}
