b2d8dd26f9600beb094c56f60917a9a52dfe9df0
김규란 <gyuraniakim@naver.com>
2022-08-10 16:10:24 +0900

    FEAT: reservation
    * 상담예약 시 입력값 변경
    * 아동이름, 보호자아이디, 상담사아이디, 예약시간
    * WebClient 적용

--- BackEnd-Service/src/main/java/com/ssafy/web/db/repository/ReservRepository.java
+++ BackEnd-Service/src/main/java/com/ssafy/web/db/repository/ReservRepository.java
@@ -10,6 +10,7 @@ import com.ssafy.web.db.entity.Reservation;
 
 public interface ReservRepository extends JpaRepository<Reservation, Integer> {
 
+	/** 상담사한테 예약된 상담 조회 */
 	@Query(value = "select * from reservation where thera_id :thera_id", nativeQuery = true)
 	public List<Reservation> selectAllSQL(@Param(value = "thera_id") String thera_id);
 
--- BackEnd-Service/src/main/java/com/ssafy/web/service/ReservServiceImpl.java
+++ BackEnd-Service/src/main/java/com/ssafy/web/service/ReservServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.ssafy.web.db.entity.Reservation;
 import com.ssafy.web.db.repository.ReservRepository;
@@ -15,11 +16,22 @@ public class ReservServiceImpl implements ReservService {
 	@Autowired
 	ReservRepository reservRepository;
 
+	@Autowired
+	WebClient webClient;
+
 	@Override
 	public void reservRegist(ReservRequest reservInfo) {
 		Reservation reserv = new Reservation();
 
-//		reserv.setChildId(reservInfo.getChild_name());
+		// auth-api에서 아동 아이디 가져오기
+		String childName = reservInfo.getChild_name();
+		String parentId = reservInfo.getParent_id();
+
+		String childId = webClient.get().uri("/child/" + parentId + "/" + childName).retrieve().bodyToMono(String.class)
+				.block();
+		System.out.println("아동 아이디: " + childId);
+
+		reserv.setChildId(childId);
 		reserv.setParentId(reservInfo.getParent_id());
 		reserv.setTheraId(reservInfo.getThera_id());
 		reserv.setReservTime(reservInfo.getReserv_time());
