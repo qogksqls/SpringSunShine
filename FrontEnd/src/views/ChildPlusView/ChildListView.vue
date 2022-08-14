9caa328d3277a171ddb1f41c586e08760490821f
Lee Bom <Sbome0214@naver.com7>
2022-08-15 03:38:09 +0900

    REFACTOR | 자녀추가페이지 반응형 고려

--- FrontEnd/src/views/ChildPlusView/ChildListView.vue
+++ FrontEnd/src/views/ChildPlusView/ChildListView.vue
@@ -5,23 +5,23 @@
     </section>
     <section class="section section-skew">
       <div class="container">
-        <card shadow class="px-5 px-lg-5 card-profile mt--300" no-body>
+        <card shadow class="px-lg-5 card-profile mt--300" no-body>
           <!--상담일지목록 시작-->
-          <div class="wrap_content col col-lg-12 p-5">
+          <div class="wrap_content col col-lg-12 p-md-5">
             <!--위에 타이틀 부분-->
-            <div class="col-lg-12 row mb-2">
-              <h3 class="col-lg-10">
-                <div class="text-muted text-left mb-3">
+            <div class="col-md-12 row m-2">
+              <h3>
+                <div class="text-muted text-left my-3">
                   <b style="color:#8898aa;">자녀관리</b>
                 </div>
               </h3>
 
               <!--추가버튼-->
-              <div class="chogabtn row col-lg-2 justify-content-center">
+              <div class="chogabtn row col-md-2 col justify-content-center">
                 <button class="childbtn" @click="아동추가 = true">
                   <i class="fa fa-user-plus fa-3x" aria-hidden="true"></i>
 
-                  <div class="col-sm-12 justify-content-center text-center">
+                  <div class="row justify-content-center text-center">
                     <b>아동추가</b>
                   </div>
                 </button>
@@ -30,48 +30,60 @@
             </div>
             <!--위 타이틀 완료-->
 
-            <div class="children row col-lg-12">
+            <div class="children col">
               <div
                 v-for="(child, i) in children"
                 :key="i"
-                class="col-lg-4 mb-5"
+                class="col-sm-4 mb-5"
               >
                 <div class="child py-3">
-                  <h4 class="col col-md-12">
+                  <h4 class="col col-sm-12">
                     <!-- <p>{{child["profileUrl"].slice(5, child["profileUrl"].length)}}</p> -->
                     <p>이름: <nbsp></nbsp>{{ child["name"] }}</p>
                     <p>생년월일: {{ child["birth"].slice(0, 10) }}</p>
-                    <p>성별: {{ child["gender"]}}</p>
+                    <p>성별: {{ child["gender"] }}</p>
                   </h4>
                   <div
-                    class="row text-center justify-content-center col-lg-12 mx-0"
+                    class="row text-center justify-content-center col-sm-12 mx-0"
                   >
-                    <div class="col-lg-6 px-0" v-if="child['surveyFlag']">
-                      <base-button type="default" class="childbutton" @click="moveSurveyResult(i)">
+                    <div class="col-sm-6 px-0" v-if="child['surveyFlag']">
+                      <base-button
+                        type="default"
+                        class="childbutton"
+                        @click="moveSurveyResult(i)"
+                      >
                         문진표보기
                       </base-button>
                     </div>
-                    <div class="col-lg-6 px-0" v-else>
-                      <base-button type="default" class="childbutton" @click="moveSurvey(i)">
+                    <div class="col-sm-6 px-0" v-else>
+                      <base-button
+                        type="default"
+                        class="childbutton"
+                        @click="moveSurvey(i)"
+                      >
                         문진표작성
                       </base-button>
                     </div>
-                    <div class="col-lg-6 px-0" v-if="child['surveyFlag']">
-                      <base-button type="default" class="childbutton" @click="moveCounselorRecom(i)">
+                    <div class="col-sm-6 px-0" v-if="child['surveyFlag']">
+                      <base-button
+                        type="default"
+                        class="childbutton"
+                        @click="moveCounselorRecom(i)"
+                      >
                         상담사추천
                       </base-button>
                     </div>
-                    <div class="col-lg-6 px-0" v-else>
+                    <div class="col-sm-6 px-0" v-else>
                       <go-to-survey></go-to-survey>
                     </div>
 
-                    <router-link to="/childReserveShow" class="col-lg-6 px-0">
+                    <router-link to="/childReserveShow" class="col-sm-6 px-0">
                       <base-button type="default" class="childbutton">
                         상담 내역
                       </base-button>
                     </router-link>
 
-                    <router-link to="/survey" class="col-lg-6 px-0">
+                    <router-link to="/survey" class="col-sm-6 px-0">
                       <base-button type="default" class="childbutton">
                         놀이 기록
                       </base-button>
@@ -80,14 +92,10 @@
                 </div>
               </div>
               <div class="childcard col-lg-4 mb-5 py-3" v-if="아동추가 == true">
-                <div class="col-md-12">
-                  <div>
-                    <p class="col-md-6">프로필</p>
-                    <img
-                      v-if="image"
-                      :src="image"
-                      width="100"
-                      height="100">
+                <div class="col-sm-12">
+                  <div class="row mb-4">
+                    <p class="col-sm-5 m-0">프로필사진</p>
+
                     <input
                       ref="image"
                       @change="uploadImg()"
@@ -95,36 +103,39 @@
                       id="chooseFile"
                       name="chooseFile"
                       accept="image/*"
-                    >
+                      class="col-sm-7 text-right align-self-end"
+                    />
+                    <div class="col-sm-5"></div>
+                    <img v-if="image" :src="image" width="100" height="100" />
                   </div>
-                  <div class="row">
-                    <p class="col-md-5">이름</p>
+                  <div class="row mb-2">
+                    <p class="col-sm-5">이름</p>
                     <base-input
                       placeholder="이름을 적어주세요"
                       type="text"
                       v-model="name"
-                      class="col-md-7"
+                      class="col-sm-7"
                     />
                   </div>
 
-                  <div for="cer_get" class="row">
-                    <p class="col-md-8">생년월일</p>
+                  <div for="cer_get" class="row mb-2">
+                    <p class="col-sm-5">생년월일</p>
                     <base-input
                       type="date"
                       id="cer_get"
                       v-model="birth"
-                      class="col-md-12"
+                      class="col-sm-7"
                     />
                   </div>
-                  <div class="row">
-                    <p class="col-md-5">
+                  <div class="row mb-2">
+                    <p class="col-sm-5">
                       성별
                     </p>
                     <select
                       name="gender"
                       id="gender"
                       v-model="gender"
-                      class="ml-3 col-md-6 form-control"
+                      class="ml-3 col-sm-6 form-control"
                     >
                       <option value="" selected>선택</option>
                       <option value="남자">남</option>
@@ -133,9 +144,7 @@
                   </div>
                 </div>
                 <div class="text-center justify-content-center my-3">
-                  <base-button
-                    type="primary"
-                    @click="addChild">
+                  <base-button type="primary" @click="addChild">
                     <b>완료</b>
                   </base-button>
                   <base-button type="primary" @click="아동추가 = false">
@@ -152,13 +161,13 @@
 </template>
 
 <script>
-import axios from 'axios'
-import GoToSurvey from '../../components/Reserve/GoToSurvey.vue';
+import axios from "axios";
+import GoToSurvey from "../../components/Reserve/GoToSurvey.vue";
 
 // var frm = new FormData();
 
 export default {
-  name: 'ChildListView',
+  name: "ChildListView",
   components: { GoToSurvey },
   data() {
     return {
@@ -172,9 +181,10 @@ export default {
   },
   methods: {
     addChild() {
-      console.log("아동추가")
+      console.log("아동추가");
       if (
-        this.name.length == 0 ||        this.birth.length == 0 ||
+        this.name.length == 0 ||
+        this.birth.length == 0 ||
         this.gender.length == 0 ||
         this.image == ""
       ) {
@@ -187,37 +197,37 @@ export default {
         //   }
         // })
         axios({
-          url: 'https://i7a606.q.ssafy.io/service-api/child/register',
-          method: 'post',
+          url: "https://i7a606.q.ssafy.io/service-api/child/register",
+          method: "post",
           data: {
-            "parent_id": this.$store.state.accounts.userid,
-            "name": this.name,
-            "birth": this.birth,
-            "gender": this.gender,
-            "profile_url": this.image,
-            "survey_flag": 0
-          }
+            parent_id: this.$store.state.accounts.userid,
+            name: this.name,
+            birth: this.birth,
+            gender: this.gender,
+            profile_url: this.image,
+            survey_flag: 0,
+          },
         })
-          .then(res => {
-            console.log(res.data)
-            router.push({ name: 'children' })
+          .then((res) => {
+            console.log(res.data);
+            router.push({ name: "children" });
           })
-          .catch(err => {
-            console.log(err.response)
-          })
-          this.아동추가 = false;
-          this.name = "",
-          this.birth = "",
-          this.gender = "",
-          this.image = ""
+          .catch((err) => {
+            console.log(err.response);
+          });
+        this.아동추가 = false;
+        (this.name = ""),
+          (this.birth = ""),
+          (this.gender = ""),
+          (this.image = "");
       }
     },
     uploadImg() {
-      var image = this.$refs['image'].files[0]
+      var image = this.$refs["image"].files[0];
       // this.image = image
-      const url = URL.createObjectURL(image)
-      this.image = url
-      console.log(this.image)
+      const url = URL.createObjectURL(image);
+      this.image = url;
+      console.log(this.image);
     },
     // filePreview() {
     //   return "data:image/jpeg;base64," + this.empPhoto;
@@ -226,30 +236,43 @@ export default {
       this.$router.push({ name: "survey", params: this.children[index] });
     },
     moveCounselorRecom(index) {
-      this.$router.push({ name: "counselorRecommend", params: this.children[index] });
+      this.$router.push({
+        name: "counselorRecommend",
+        params: this.children[index],
+      });
     },
     moveSurveyResult(index) {
       this.$router.push({ name: "surveyresult", params: this.children[index] });
-    }
+    },
   },
   created() {
-    console.log(`userid: ${this.$store.state.accounts.userid}`)
+    console.log(`userid: ${this.$store.state.accounts.userid}`);
     axios({
-      url: `https://i7a606.q.ssafy.io/service-api/child/${this.$store.state.accounts.userid}`,
-      method: 'get',
+      url: `https://i7a606.q.ssafy.io/service-api/child/${
+        this.$store.state.accounts.userid
+      }`,
+      method: "get",
     })
-      .then(res => {
-        console.log(res.data)
-        this.children = res.data
+      .then((res) => {
+        console.log(res.data);
+        this.children = res.data;
       })
-      .catch(err => {
-        console.log(err.response)
-      })
-  }
+      .catch((err) => {
+        console.log(err.response);
+      });
+  },
 };
 </script>
 
 <style scoped>
+@media (min-width: 768px) and (max-width: 991px) {
+  .card {
+    height: 100vh;
+  }
+}
+.card {
+  position: relative;
+}
 .child {
   border: 1px solid #dcdcdc;
   border-radius: 15px;
@@ -273,7 +296,6 @@ button:active {
   outline: none;
   box-shadow: none;
 }
-
 .fa-user-plus {
   color: #8898aa;
 }
