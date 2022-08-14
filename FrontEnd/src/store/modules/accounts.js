ac91e49bc92a1ba63659fe951597ea40aee4681e
Lee Bom <Sbome0214@naver.com7>
2022-08-14 23:26:25 +0900

    FIX | 오류 수정

--- FrontEnd/src/store/modules/accounts.js
+++ FrontEnd/src/store/modules/accounts.js
@@ -1,10 +1,11 @@
-import axios from "axios"
+import axios from "axios";
+import router from "../../router";
 
 export default {
   state: {
-    accessToken: localStorage.getItem('accessToken') || '',
-    refreshToken: localStorage.getItem('refreshToken') || '',
-    userid: localStorage.getItem('userid') || '',
+    accessToken: localStorage.getItem("accessToken") || "",
+    refreshToken: localStorage.getItem("refreshToken") || "",
+    userid: localStorage.getItem("userid") || "",
     // accessToken: '',
     // refreshToken: '',
     // userid: '',
@@ -13,142 +14,130 @@ export default {
     authError: null,
   },
   getters: {
-    isLoggedIn: state => !!state.refreshToken,
-    currentUser: state => state.currentUser,
-    profile: state => state.profile,
-    authError: state => state.authError
+    isLoggedIn: (state) => !!state.refreshToken,
+    currentUser: (state) => state.currentUser,
+    profile: (state) => state.profile,
+    authError: (state) => state.authError,
   },
   mutations: {
-    SET_Access_TOKEN: (state, accessToken) => state.accessToken = accessToken,
-    SET_Refresh_TOKEN: (state, refreshToken) => state.refreshToken = refreshToken,
-    SET_USERID: (state, userid) => state.userid = userid,
-    SET_CURRENT_USER: (state, user) => state.currentUser = user,
-    SET_PROFILE: (state, profile) => state.profile = profile,
-    SET_AUTH_ERROR: (state, error) => state.authError = error,
+    SET_Access_TOKEN: (state, accessToken) => (state.accessToken = accessToken),
+    SET_Refresh_TOKEN: (state, refreshToken) =>
+      (state.refreshToken = refreshToken),
+    SET_USERID: (state, userid) => (state.userid = userid),
+    SET_CURRENT_USER: (state, user) => (state.currentUser = user),
+    SET_PROFILE: (state, profile) => (state.profile = profile),
+    SET_AUTH_ERROR: (state, error) => (state.authError = error),
   },
   actions: {
     saveAccessToken({ commit }, accessToken) {
-      commit('SET_Access_TOKEN', accessToken)
-      localStorage.setItem('accessToken', accessToken)
+      commit("SET_Access_TOKEN", accessToken);
+      localStorage.setItem("accessToken", accessToken);
       // console.log(localStorage)
     },
-    getters: {
-        isLoggedIn: state => !!state.token,
-        currentUser: state => state.currentUser,
-        profile: state => state.profile,
-        authError: state => state.authError,
-        // authHeader: state => ({ Authorization: `Token ${state.token}`}),
+    saveRefreshToken({ commit }, refreshToken) {
+      commit("SET_Refresh_TOKEN", refreshToken);
+      localStorage.setItem("refreshToken", refreshToken);
+      // console.log(localStorage)
     },
     saveUserid({ commit }, userid) {
-      commit('SET_USERID', userid)
-      localStorage.setItem('userid', userid)
+      commit("SET_USERID", userid);
+      localStorage.setItem("userid", userid);
     },
     removeToken({ commit }) {
-      commit('SET_Access_TOKEN', '')
-      commit('SET_Refresh_TOKEN', '')
-      localStorage.setItem('accessToken', '')
-      localStorage.setItem('refreshToken', '')
+      commit("SET_Access_TOKEN", "");
+      commit("SET_Refresh_TOKEN", "");
+      localStorage.setItem("accessToken", "");
+      localStorage.setItem("refreshToken", "");
     },
     removeUserid({ commit }) {
-      commit('SET_USERID', '')
-      localStorage.setItem('userid', '')
+      commit("SET_USERID", "");
+      localStorage.setItem("userid", "");
     },
     login({ commit, dispatch }, credentials) {
-      console.log("로그인")
-      console.log(credentials)
+      console.log("로그인");
+      console.log(credentials);
       // if (credentials.saveId) {
       //   this.$cookies.set("idCookie", credentials.id);
       // }
       axios({
-        url: 'https://i7a606.q.ssafy.io/auth-api/auth/login',
-        method: 'post',
-        data: credentials
+        url: "https://i7a606.q.ssafy.io/auth-api/auth/login",
+        method: "post",
+        data: credentials,
       })
-        .then(res => {
-          console.log(res.data)
-          const accessToken = res.data.accessToken
-          const refreshToken = res.data.refreshToken
-          const userid = res.data.userid
-          dispatch('saveAccessToken', accessToken)
-          dispatch('saveRefreshToken', refreshToken)
-          dispatch('saveUserid', userid)
-          dispatch('fetchCurrentUser')
-          router.push({ name: 'components' })
+        .then((res) => {
+          console.log(res.data);
+          const accessToken = res.data.accessToken;
+          const refreshToken = res.data.refreshToken;
+          const userid = res.data.userid;
+          dispatch("saveAccessToken", accessToken);
+          dispatch("saveRefreshToken", refreshToken);
+          dispatch("saveUserid", userid);
+          dispatch("fetchCurrentUser");
+          router.push({ name: "components" });
         })
-        .catch(err => {
-          console.log(err.response)
-          commit('SET_AUTH_ERROR', err.response.data)
-        })
+        .catch((err) => {
+          console.log(err.response);
+          commit("SET_AUTH_ERROR", err.response.data);
+        });
     },
     logout({ dispatch }) {
       // console.log(this.state.accounts.accessToken)
       if (!this.state.accounts.accessToken) {
-        router.push({ name: 'login' })
+        router.push({ name: "login" });
       }
       axios({
-        url: 'https://i7a606.q.ssafy.io/auth-api/auth/logout',
-        method: 'post',
+        url: "https://i7a606.q.ssafy.io/auth-api/auth/logout",
+        method: "post",
         data: {
-          "accessToken": this.state.accounts.accessToken,
-          "refreshToken": this.state.accounts.refreshToken
-        }
+          accessToken: this.state.accounts.accessToken,
+          refreshToken: this.state.accounts.refreshToken,
+        },
       })
-        .then(res => {
-          dispatch('removeToken')
-          dispatch('removeUserid')
-          alert("로그아웃 되었습니다.")
-          router.push({ name: 'login' })
+        .then((res) => {
+          dispatch("removeToken");
+          dispatch("removeUserid");
+          alert("로그아웃 되었습니다.");
+          router.push({ name: "login" });
         })
-        .catch(err => {
-          console.log("로그아웃 실패!")
-          console.log(err.response)
-        })
+        .catch((err) => {
+          console.log("로그아웃 실패!");
+          console.log(err.response);
+        });
     },
-    actions: {
-        saveToken({ commit }, token) {
-            commit('SET_TOKEN', token)
-            localStorage.setItem('token', token)
-        },
-        removeToken({ commit }) {
-            commit('SET_TOKEN', '')
-            localStorage.setItem('token', '')
-        },
-
-        login({ commit, dispatch }, credentials) {
-            axios({
-                url: `${state.host}/auth-api/auth/login`,
-                method: 'post',
-                data: credentials
-            })
-            .then(res => {
-                const token = res.data.key
-                console.log(token)
-                dispatch('saveToken', token)
-                dispatch('fetchCurrentUser')
-                router.push({ name: 'components' })
-            })
-            .catch(err => {
-                console.log(err.response.data)
-                commit('SET_AUTH_ERROR', err.response.data)
-            })
-        },
-
-        fetchCurrentUser({ commit, getters, dispatch }) {
-            if (getters.isLoggedIn) {
-              axios({
-                url: `${state.host}/auth-api/user/${state.userid}`,
-                method: 'get',
-              })
-                .then(res => commit('SET_CURRENT_USER', res.data, console.log(res)))
-                .catch(err => {
-                  if (err.response.status === 401) {
-                    dispatch('removeToken')
-                    router.push({ name: 'login' })
-                  }
-                })
+    fetchCurrentUser({ commit, getters, dispatch }) {
+      if (getters.isLoggedIn) {
+        axios({
+          url: `https://i7a606.q.ssafy.io/service-api/user/${this.userid}`,
+          method: "get",
+        })
+          .then((res) => {
+            console.log(res.data);
+            commit("SET_CURRENT_USER", res.data);
+          })
+          .catch((err) => {
+            console.log(err.response);
+            if (err.response.status === 401) {
+              dispatch("removeToken");
+              router.push({ name: "components" });
             }
-        },
+          });
+      }
     },
-}
-  
-  export default userStore
+    // signup({ commit, dispatch }, credentials) {
+    //   console.log(credentials)
+    //   axios({
+    //     url: `${this.$store.state.host}/auth-api/user/therapist`,
+    //     method: 'post',
+    //     data: credentials
+    //   })
+    //     .then(res => {
+    //       console.log(res.data)
+    //       router.push({ name: 'login' })
+    //     })
+    //     .catch(err => {
+    //       console.log(err)
+    //       commit('SET_AUTH_ERROR', err.response.data)
+    //     })
+    // },
+  },
+};
