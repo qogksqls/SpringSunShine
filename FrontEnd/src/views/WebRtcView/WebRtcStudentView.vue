36a3859a6fba4f1daeca9d233db66faf061150b4
JunOnJuly <wlgnstjd0717@gmail.com>
2022-08-15 21:52:57 +0900

    FEAT|update url

--- FrontEnd/src/views/WebRtcView/WebRtcStudentView.vue
+++ FrontEnd/src/views/WebRtcView/WebRtcStudentView.vue
@@ -79,7 +79,7 @@ import CardsComp from '@/components/webRtcComp/CardsComp.vue'
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 // const OPENVIDU_SERVER_URL = "https://" + location.hostname + ":4443";
-const OPENVIDU_SERVER_URL = "i7a606.q.ssafy.io:8443" ;
+const OPENVIDU_SERVER_URL = "https://i7a606.q.ssafy.io:8443" ;
 
 // const OPENVIDU_SERVER_SECRET = "MY_SECRET";
 const OPENVIDU_SERVER_SECRET = "A606";
