654d6afec8c96a9a7ac461f49f1a67a953508d88
이준경 <away0419@naver.com>
2022-08-16 23:40:50 +0900

    FIX : image

--- BackEnd/src/main/java/com/ssafy/web/service/RecommendServiceImpl.java
+++ BackEnd/src/main/java/com/ssafy/web/service/RecommendServiceImpl.java
@@ -1,17 +1,10 @@
 package com.ssafy.web.service;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.ServletContext;
-
-import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Service;
 
-import com.ssafy.web.common.PathUtil;
 import com.ssafy.web.db.entity.Therapist;
 import com.ssafy.web.db.repository.TheraRepository;
 import com.ssafy.web.model.response.RecommendTherapistResponse;
@@ -23,7 +16,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class RecommendServiceImpl implements RecommendService{
 	private final TheraRepository threpo;
-	private final ServletContext servletContext;
 
 	@Override
 	public List<RecommendTherapistResponse> findByUser_UserIdIn(String[] thera_id){
@@ -31,24 +23,7 @@ public class RecommendServiceImpl implements RecommendService{
 		List<RecommendTherapistResponse> rlist = new ArrayList<RecommendTherapistResponse>();
 		
 		list.stream().forEach((e)->{
-			try {
 				rlist.add(new RecommendTherapistResponse(e));
-				RecommendTherapistResponse r = rlist.get(rlist.size()-1);
-				if (e.getProfileUrl() != null) {
-					String str = servletContext.getRealPath(PathUtil.PROFILE_PATH);
-	    			String url = str+e.getProfileUrl();
-	    			System.out.println(url);
-	    			InputStream imageIS = new FileInputStream(url);
-	    			byte[] imageByteArray = IOUtils.toByteArray(imageIS);
-	    			r.setProfile_url(imageByteArray);
-	    			imageIS.close();
-
-	    		}
-				
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
 		});
 		
 		return rlist;
@@ -60,24 +35,7 @@ public class RecommendServiceImpl implements RecommendService{
 		List<RecommendTherapistResponse> rlist = new ArrayList<RecommendTherapistResponse>();
 		
 		list.stream().forEach(e->{
-			try {
 				rlist.add(new RecommendTherapistResponse(e));
-				RecommendTherapistResponse r = rlist.get(rlist.size()-1);
-				if (e.getProfileUrl() != null) {
-					String str = servletContext.getRealPath(PathUtil.PROFILE_PATH);
-	    			String url = str+e.getProfileUrl();
-	    			System.out.println(url);
-
-	    			InputStream imageIS = new FileInputStream(url);
-	    			byte[] imageByteArray = IOUtils.toByteArray(imageIS);
-	    			r.setProfile_url(imageByteArray);
-	    			imageIS.close();
-
-	    		}
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
 		});
 		
 		return rlist;
