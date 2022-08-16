0954e18cc2acaa94f3204f994e2c09a0502e6e5c
이준경 <away0419@naver.com>
2022-08-16 17:06:34 +0900

    FEAT : cors

--- BackEnd-Service/src/main/java/com/ssafy/web/config/WebClientConfig.java
+++ BackEnd-Service/src/main/java/com/ssafy/web/config/WebClientConfig.java
@@ -3,6 +3,9 @@ package com.ssafy.web.config;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -12,5 +15,19 @@ public class WebClientConfig {
 	@Bean
 	public WebClient webClient() {
 		return WebClient.create("https://i7a606.q.ssafy.io/auth-api");
+	}
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.addExposedHeader("*");
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+        source.registerCorsConfiguration("/**", configuration);
+        return new CorsFilter(source);
+
 	}
 }
