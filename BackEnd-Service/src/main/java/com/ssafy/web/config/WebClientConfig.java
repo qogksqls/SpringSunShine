9bbcc8ac1c86663b08871e39f20fad9469f8b6b3
이준경 <away0419@naver.com>
2022-08-16 18:21:56 +0900

    REPACTOR : cors

--- BackEnd-Service/src/main/java/com/ssafy/web/config/WebClientConfig.java
+++ BackEnd-Service/src/main/java/com/ssafy/web/config/WebClientConfig.java
@@ -3,6 +3,9 @@ package com.ssafy.web.config;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
