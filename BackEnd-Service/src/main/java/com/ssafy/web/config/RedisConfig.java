e584f6631b93c6c298f7ca1463f717c8c08954a4
최지혜 <chlwlgp11@naver.com>
2022-08-17 09:30:08 +0900

    FIX:redis-auth
    redis auth 비밀번호 설정

--- BackEnd-Service/src/main/java/com/ssafy/web/config/RedisConfig.java
+++ BackEnd-Service/src/main/java/com/ssafy/web/config/RedisConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -23,11 +24,20 @@ public class RedisConfig {
 	@Value("${spring.redis.port}")
 	private int redisPort;
 	
+	@Value("${spring.redis.password}")
+	private String redisPassword;
+
+	
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory(redisHost, redisPort);
+//		return new LettuceConnectionFactory(redisHost, redisPort);
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+		redisStandaloneConfiguration.setHostName(redisHost);
+		redisStandaloneConfiguration.setPort(redisPort);
+        redisStandaloneConfiguration.setPassword(redisPassword); //redis에 비밀번호가 설정 되어 있는 경우 설정해주면 됩니다.
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+        return lettuceConnectionFactory;
 	}
-
 	//별도의 설정없이 RedisTemplate 메서드로 Redis 서버에 명령어 수행 가능 
 	@Bean
 	public RedisTemplate<String, String> redisTemplate(){
